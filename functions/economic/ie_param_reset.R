271ee0d82b4eb3a2a1114e37683970d4a10108e2
mle2718 <Min-Yang.Lee@noaa.gov>
2024-06-21 12:25:34 -0500

    modify to set and reset the catchdeviation parameters

--- functions/economic/ie_param_reset.R
+++ functions/economic/ie_param_reset.R
@@ -1,4 +1,4 @@
-# Function reset the scalar ie_F and ie_bias values to previously saved params.
+# Function reset the scalar ie_F, ie_bias, iecl_lower, and iecl_upper values to previously saved params.
 # See ie_param_override()
 
 # Usage: 
@@ -12,6 +12,10 @@ ie_param_reset <- function(stock){
   out <- within(stock, {
     ie_F<-ie_F_OG
     ie_bias<-ie_bias_OG
+    iecl_lower <-iecl_lower_OG
+    iecl_upper <-iecl_upper_OG
+    
+    
   })
   
   return(out)
--- functions/economic/ie_param_save.R
+++ functions/economic/ie_param_save.R
@@ -1,9 +1,12 @@
-# Function to save the scalar ie_F and ie_bias values in "stock" to the new names with _OG suffixes
+# Function to save the scalar ie_F, ie_bias, iecl_lower, and iecl_upper values in "stock" to the new names with _OG suffixes
 ie_param_save <- function(stock){
   
   out <- within(stock, {
     ie_F_OG<- ie_F
     ie_bias_OG<- ie_bias
+    iecl_lower_OG <- iecl_lower 
+    iecl_upper_OG<- iecl_upper 
+    
     
   })
   
